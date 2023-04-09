ba9e381b65ad554b1bb84d599ca8414d481ef561
Ivan Ovcharov <xxovcharovxx@gmail.com>
2023-04-09 15:41:28 +0300

    basic functional

--- lib/common_widgets/task/task_widget.dart
+++ lib/common_widgets/task/task_widget.dart
@@ -1,31 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
-import 'package:todo_app/models/task.dart';
+import 'package:todo_app/domain/bloc/tasks_bloc.dart';
+import 'package:todo_app/domain/models/task.dart';
 import 'package:todo_app/utils/app_colors.dart';
 import 'package:todo_app/utils/app_text_styles.dart';
 
 class TaskWidget extends StatelessWidget {
+  const TaskWidget({super.key, required this.task});
+
   final Task task;
 
-  const TaskWidget({super.key, required this.task});
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(
-          width: 334.w + 16.w,
-          height: 60.h + 5.h,
+        SizedBox(
+          width: 350.w,
+          height: 65.h,
           child: Row(
             children: [
               Transform.scale(
                 scale: 1.2,
                 child: Checkbox(
-                  value: false,
+                  value: task.isDone,
                   activeColor: AppColors.black,
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6.0),
+                    borderRadius: BorderRadius.circular(6.0.r),
                   ),
                   side: MaterialStateBorderSide.resolveWith(
                     (states) => const BorderSide(
@@ -33,29 +36,38 @@ class TaskWidget extends StatelessWidget {
                       color: Color.fromRGBO(232, 232, 232, 1),
                     ),
                   ),
-                  onChanged: (value) {},
+                  onChanged: (bool? value) {
+                    context.read<TasksBloc>().add(
+                          DoneChange(
+                            task: task,
+                          ),
+                        );
+                  },
                 ),
               ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
+                  SizedBox(
                     width: 300.w,
                     child: Text(
                       task.title,
-                      style: AppTextStyles.pageTextStyle,
+                      overflow: TextOverflow.ellipsis,
+                      style: !task.isDone
+                          ? AppTextStyles.pageTextStyle
+                          : AppTextStyles.pageTextStyleLined,
                       maxLines: 2,
                     ),
                   ),
+                  SizedBox(height: 5.h),
                   SizedBox(
-                    height: 5.h,
-                  ),
-                  Container(
                     width: 121.w,
                     child: Text(
-                      '${DateFormat('dd.MM.yyyy').format(task.date)}  ${task.time.hour.toString().padLeft(2, '0')}:${task.time.minute.toString().padLeft(2, '0')}',
-                      style: AppTextStyles.dateTextStyle,
+                      '${DateFormat('dd.MM.yyyy  HH:mm').format(task.date)} ',
+                      style: !task.isDone
+                          ? AppTextStyles.dateTextStyle
+                          : AppTextStyles.dateTextStyleLined,
                     ),
                   )
                 ],
@@ -63,9 +75,7 @@ class TaskWidget extends StatelessWidget {
             ],
           ),
         ),
-        SizedBox(
-          height: 21.h - 5.h,
-        ),
+        SizedBox(height: 16.h),
       ],
     );
   }
